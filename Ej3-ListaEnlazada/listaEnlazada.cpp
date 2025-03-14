af3177b08dc97ec7ca3a0f01faeea2f43df265f8
luca_camiscia <camiscialuca@gmail.com>
2025-03-13 23:22:17 -0300

    Avance ej 3

--- Ej3-ListaEnlazada/listaEnlazada.cpp
+++ Ej3-ListaEnlazada/listaEnlazada.cpp
@@ -1,64 +1,240 @@
 #include <iostream>
+#include <memory>
 using namespace std;
+
 struct node;
 typedef struct node node_t;
 typedef struct list list_t;
-bool push_front(void* val, list_t lista);
 
+bool push_front(void* val, unique_ptr<list_t>& lista);
+bool push_back(void * val, unique_ptr<list_t>& lista);
+bool insert(void * val, unique_ptr<list_t>& lista,unsigned pos);
+bool erase_front(unique_ptr<list_t>& lista);
+bool erase_back(unique_ptr<list_t>& lista);
+bool erase(unique_ptr<list_t>& lista, unsigned pos);
+void print_list(unique_ptr<list_t>& lista);
+void free_list(unique_ptr<list_t>& lista);
 struct node {
-    void* value;
-    node_t* next;
+    unique_ptr<void*> value;
+    unique_ptr<node_t> next;
 };
 
-struct list{
-    node_t* head;
-    node_t* tail;
+struct list {
+    unique_ptr<node_t> head;
+    node_t* tail; // tail does not need to be a unique_ptr
     unsigned size;
 };
 
-node_t* create_node(void*val);
-
-int main (void);
-
-
-node_t* create_node(void* val){
-    node_t* new_node = new (nothrow) node_t;
-    if (new_node == NULL){
-        cout << "Error al crear un nodo" <<endl;
-        return NULL;
+unique_ptr<node_t> create_node(void* val);
+unique_ptr<list_t> create_list(void);
+int main(void) {
+    unique_ptr<list_t> lista = create_list();
+    if (!lista) {
+        return -1;
     }
-    new_node->value = val;
+
+    int val1 = 1, val2 = 2, val3 = 3, val4 = 4, val5 = 5;
+
+    // Insert elements at the front
+    push_front(&val1, lista);
+    push_front(&val2, lista);
+    push_front(&val3, lista);
+    print_list(lista);
+
+    // Insert elements at the back
+    push_back(&val4, lista);
+    push_back(&val5, lista);
+    print_list(lista);
+
+    // Insert element at position 2
+    int val6 = 6;
+    insert(&val6, lista, 2);
+    print_list(lista);
+
+    // Erase front element
+    erase_front(lista);
+    print_list(lista);
+
+    // Erase back element
+    erase_back(lista);
+    print_list(lista);
+
+    // Erase element at position 2
+    erase(lista, 2);
+    print_list(lista);
+
+    // Free the list
+    free_list(lista);
+
+    return 0;
+}
+
+unique_ptr<node_t> create_node(void* val) {
+    unique_ptr<node_t> new_node = make_unique<node_t>();
+    if (!new_node) {
+        cout << "Error al crear un nodo" << endl;
+        return nullptr;
+    }
+    new_node->value = make_unique<void*>(val);
     new_node->next = nullptr;
     return new_node;
 }
 
-list_t * create_list(void){
-    list_t* new_list = new (nothrow) list_t;
-    if (new_list == NULL){
+unique_ptr<list_t> create_list(void) {
+    unique_ptr<list_t> new_list = make_unique<list_t>();
+    if (!new_list) {
         cout << "Error al crear una lista" << endl;
-        return NULL;
+        return nullptr;
     }
-    new_list->head = NULL;
-    new_list->tail = NULL;
+    new_list->head = nullptr;
+    new_list->tail = nullptr;
     new_list->size = 0;
     return new_list;
 }
-
-bool push_front(void* val, list_t* lista){
-    node_t* new_node = create_node(val);
-    if (new_node == NULL){
+bool push_front(void* val, unique_ptr<list_t>& lista) {
+    unique_ptr<node_t> new_node = create_node(val);
+    if (!new_node) {
         cout << "Error" << endl;
         return false;
     }
-    //caso lista vacia
-    if (lista->head == NULL){
-        lista->head = new_node;
-        lista->tail = new_node;
-        lista->size ++;
-        return NULL;
+    // caso lista vacia
+    if (!lista->head) {
+        lista->head = move(new_node);
+        lista->tail = lista->head.get();
+        lista->size++;
+        return true;
     }
-    new_node->next = lista->head;
-    lista->head = new_node;
+    //caso default
+    new_node->next = move(lista->head);
+    lista->head = move(new_node);
+    lista->size++;
+    return true;
+}
+bool push_back(void * val, unique_ptr<list_t>& lista){
+    unique_ptr<node_t> new_node = create_node(val);
+    if (!new_node) {
+        cout << "Error" << endl;
+        return false;
+    }
+    // caso lista vacia
+    if (!lista->head) {
+        lista->head = move(new_node);
+        lista->tail = lista->head.get();
+        lista->size++;
+        return true;
+    }
+    // caso default
+
+        lista->tail->next = move(new_node);
+        lista->tail = lista->tail->next.get();
+        lista->size ++;
+        return true;
+}
+bool insert(void * val, unique_ptr<list_t>& lista,unsigned pos){
+    if (pos > lista->size){ //mayor al largo de la lista
+        cout << "Posicion mayor al largo de la lista -> Insertando elemento al final de la lista..." << endl;
+        return push_back(val, lista); 
+    }
+    if (pos == lista->size){
+        return push_back(val, lista);
+    }
+    if (pos == 0){
+        return push_front(val, lista);
+    }
+
+    unique_ptr<node_t> new_node = create_node(val);
+    if (!new_node) {
+        cout << "Error" << endl;
+        return false;
+    }
+    node_t* aux = lista->head.get();
+    for (int i = 0; i<(pos-1);i++){ //me paro en el nodo anterior a la posicion
+        aux = aux->next.get();
+    }
+    new_node->next = move(aux->next);
+    aux->next = move(new_node);
     lista->size ++;
     return true;
+}
+
+
+bool erase_front(unique_ptr<list_t>& lista) {
+    if (!lista->head) {
+        cout << "La lista esta vacía" << endl;
+        return false;
+    }
+    if (lista->size == 1){
+        lista->head.reset();
+        lista->tail = nullptr;
+        lista->size--;
+        return true;
+    }
+    lista->head = move(lista->head->next);
+    if (!lista->head) {
+        lista->tail = nullptr;
+    }
+    lista->size--;
+    return true;
+}
+
+bool erase_back(unique_ptr<list_t>& lista){
+    if (!lista->head){
+        cout << "La lista esta vacia" << endl;
+        return false;
+    }
+    if (lista->size == 1) {
+        lista->head.reset();
+        lista->tail = nullptr;
+        lista->size--;
+        return true;
+    }
+    //tengo que llegar hasta la pos antes de la tail
+    node_t* aux = lista->head.get();
+    for (int i = 0; i<((lista->size)-1);i++){ //me paro en el nodo anterior a la posicion
+        aux = aux->next.get();
+    }
+    aux ->next.reset();
+    lista->tail = aux;
+    lista->size --;
+    return true;
+    }
+
+bool erase(unique_ptr<list_t>& lista, unsigned pos){
+    if (pos == 0 || lista->size == 1){
+        return erase_front(lista);
+    }
+    if (pos == lista->size ){
+        return erase_back(lista);
+    }
+    if (pos > lista->size){
+        cout << "Posicion mayor al largo de la lista -> Eliminando elemento al final de la lista..." << endl;
+        return erase_back(lista);
+    }
+
+    node*aux = lista->head.get();
+    for (int i = 0;i<(pos-1);i++){
+        aux = aux->next.get();
+    }
+    aux->next = move(aux->next->next);
+    lista->size -- ;
+    return true;
+}
+void print_list(unique_ptr<list_t>& lista) {
+    if (!lista->head) {
+        cout << "La lista esta vacia" << endl;
+        return;
+    }
+    node_t* current = lista->head.get();
+    while (current) {
+        cout << *static_cast<int*>(*current->value) << " -> " ;
+        current = current->next.get();
+    }
+    cout << "NULL" << endl;
+}
+void free_list(unique_ptr<list_t>& lista) {
+    while (lista->head) {
+        lista->head = move(lista->head->next);
+    }
+    lista->tail = nullptr;
+    lista->size = 0;
 }
